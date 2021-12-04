b1e83b7d285b78a0797bc59fced85a3276b4c38f
Ivan Kiral <kiralivan@gmail.com>
2021-12-04 13:28:54 +0100

    Update contoller to use service and repository

--- API/Controllers/AstronautController.cs
+++ API/Controllers/AstronautController.cs
@@ -1,4 +1,7 @@
-﻿using API.Models;
+﻿using API.DTO;
+using API.Models;
+using API.Services;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
@@ -7,43 +10,83 @@ namespace API.Controllers;
 [Route("api/[controller]")]
 public class AstronautController: ControllerBase
 {
+    private readonly IAstronautService _astronautService;
+    private readonly IMapper _mapper;
+    
+    public AstronautController(IAstronautService astronautService, IMapper mapper)
+    {
+        _astronautService = astronautService;
+        _mapper = mapper;
+    }
+
     [HttpGet]
     [Route("")]
-    public ActionResult<AstronautAllResponseModel> GetAstronauts() => new AstronautAllResponseModel
+    public async Task<ActionResult<AstronautAllResponseModel>> GetAstronauts()
     {
-        Astroanuts = new List<AstronautResponseModel>()
-    };
+        try
+        {
+            var astronauts = _astronautService.GetALlAstronauts();
+            var astronautsList = await astronauts.ToListAsync();
+
+            return new AstronautAllResponseModel
+            {
+                Astroanuts = astronautsList
+                    .Select(astronaut => _mapper.Map<Astronaut, AstronautResponseModel>(astronaut))
+            };
+        }  catch (Exception e)
+        {
+            return BadRequest();
+        }
+    }
     
     [HttpPost]
     [Route("")]
-    public ActionResult<AstronautResponseModel> CreateNote([FromBody] AstronautRequestModel requestModel)
+    public async Task<ActionResult<AstronautResponseModel>> CreateNote([FromBody] AstronautRequestModel requestModel)
     {
-        return new AstronautResponseModel
+        try
         {
-            Name = "Test",
-            Surname = "TestSurname",
-            Birthday = DateTime.Now,
-            Ability = "TestAbility"
-        };
+            var astronautModel = _mapper.Map<AstronautRequestModel, AddAstronaut>(requestModel);
+            var astronaut = await _astronautService.AddAstronaut(astronautModel);
+
+            return _mapper.Map<Astronaut, AstronautResponseModel>(astronaut);
+        } 
+        catch (Exception e)
+        {
+            return BadRequest();
+        }
     }
 
     [HttpPut]
     [Route("{id:guid}")]
-    public ActionResult<AstronautResponseModel> UpdateNote([FromRoute] Guid id, [FromBody] AstronautRequestModel requestModel)
+    public async Task<ActionResult<AstronautResponseModel>> UpdateNote([FromRoute] Guid id,
+        [FromBody] AstronautRequestModel requestModel)
     {
-        return new AstronautResponseModel
+        try
         {
-            Name = "Test",
-            Surname = "TestSurname",
-            Birthday = DateTime.Now,
-            Ability = "TestAbility"
-        };
-    }
+            var astronautModel = _mapper.Map<AstronautRequestModel, AddAstronaut>(requestModel);
+            var astronaut = await _astronautService.AddAstronaut(astronautModel);
+
+            return _mapper.Map<Astronaut, AstronautResponseModel>(astronaut);
+        }
+        catch (Exception e)
+        {
+            return BadRequest();
+        }
+}
 
     [HttpDelete]
     [Route("{id:guid}")]
-    public ActionResult DeleteNote([FromRoute] Guid id)
+    public async Task<ActionResult> DeleteNote([FromRoute] Guid id)
     {
-        return Ok();
+        try
+        {
+            await _astronautService.DeleteAstronaut(id);
+
+            return Ok();
+        }
+        catch (Exception e)
+        {
+            return BadRequest();
+        }
     }
 }
