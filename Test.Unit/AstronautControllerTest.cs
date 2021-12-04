28816a0ed17d9e982b3c7ce9dec909b23ee051e0
Ivan Kiral <kiralivan@gmail.com>
2021-12-04 19:14:37 +0100

    Add controller tests

---
+++ Test.Unit/AstronautControllerTest.cs
@@ -0,0 +1,125 @@
+﻿using API.Controllers;
+using AutoMapper;
+using API.DTO;
+using AstronautDao = API.DAO.Astronaut;
+using API.Services;
+using API.Utils;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+using Xunit;
+using Test.Unit.Helpers;
+using static Test.Unit.Helpers.AstronautHelpers;
+using Mapper = API.Configuration.Mapper;
+
+namespace Test.Unit;
+
+public class AstronautControllerTest
+{
+    private readonly IMapper _mapper = Mapper.GetMapperInstance();
+    
+    private static readonly IAsyncEnumerable<Astronaut> Astronauts = GetAstronautDtosAsync();
+    
+    [Fact]
+    public async void GetAstronauts_ReturnsAstroanuts()
+    {
+        var service = Substitute.For<IAstronautService>();
+        service.GetALlAstronauts().Returns(Astronauts);
+        var controller = new AstronautController(service, _mapper);
+            
+        var result = await controller.GetAstronauts();
+
+        var expectedAstronauts = await Astronauts.ToListAsync();
+        var expectedResponse = ToAstronautAllResponse(expectedAstronauts);
+
+        result.Value.Should().BeEquivalentTo(expectedResponse);
+    }
+
+    [Fact]
+    public async void AddAstronaut_ReturnsAstronaut()
+    {
+        var service = Substitute.For<IAstronautService>();
+        service.AddAstronaut(Arg.Is<AddAstronaut>(a => a.Name == MissingAstronaut.Name))
+            .Returns(AstronautHelpers.MissingAstronaut);
+        var controller = new AstronautController(service, _mapper);
+        
+        var requestModel = ToAstronautRequest(MissingAstronaut);
+        var result = await controller.CreateAstronaut(requestModel);
+        var expectedResult = ToAstronautResponse(MissingAstronaut);
+
+        result.Value.Should().BeEquivalentTo(expectedResult);
+    }
+    
+    [Fact]
+    public async void AddAstronaut_ReturnsBadRequest()
+    {
+        var service = Substitute.For<IAstronautService>();
+        service.AddAstronaut(Arg.Is<AddAstronaut>(a => a.Name == SecondAstronaut.Name))
+            .Throws<ArgumentException>();
+        var controller = new AstronautController(service, _mapper);
+        
+        var requestModel = ToAstronautRequest(SecondAstronaut);
+        var result = await controller.CreateAstronaut(requestModel);
+
+        result.Result.Should().BeOfType<BadRequestResult>();
+    }
+    
+    
+    [Fact]
+    public async void UpdateAstronaut_ReturnsAstronaut()
+    {
+        var service = Substitute.For<IAstronautService>();
+        var serviceResultAstronaut = AstronautUtils.MakeAstronaut(
+            SecondAstronaut.Id,
+            ToAddAstronaut(MissingAstronaut)
+        );
+        service.UpdateAstronaut(SecondAstronaut.Id,
+                Arg.Is<AddAstronaut>(a => a.Name == MissingAstronaut.Name))
+            .Returns(serviceResultAstronaut);
+        var controller = new AstronautController(service, _mapper);
+        
+        var requestModel = ToAstronautRequest(MissingAstronaut);
+        var result = await controller.UpdateAstronaut(SecondAstronaut.Id, requestModel);
+        var expectedResult = ToAstronautResponse(serviceResultAstronaut);
+
+        result.Value.Should().BeEquivalentTo(expectedResult);
+    }
+    
+    [Fact]
+    public async void UpdateAstronaut_ReturnsBadRequest()
+    {
+        var service = Substitute.For<IAstronautService>();
+        service.UpdateAstronaut(MissingAstronaut.Id, Arg.Is<AddAstronaut>(a => a.Name == MissingAstronaut.Name))
+            .Throws<ArgumentException>();
+        var controller = new AstronautController(service, _mapper);
+        
+        var requestModel = ToAstronautRequest(MissingAstronaut);
+        var result = await controller.UpdateAstronaut(MissingAstronaut.Id, requestModel);
+
+        result.Result.Should().BeOfType<BadRequestResult>();
+    }
+    
+    [Fact]
+    public async void DeleteAstronaut_ReturnsOk()
+    {
+        var service = Substitute.For<IAstronautService>();
+        var controller = new AstronautController(service, _mapper);
+
+        var result = await controller.DeleteAstronaut(SecondAstronaut.Id);
+
+        result.Should().BeOfType<OkResult>();
+    }
+    
+    [Fact]
+    public async void DeleteAstronaut_ReturnsBadRequest()
+    {
+        var service = Substitute.For<IAstronautService>();
+        service.DeleteAstronaut(MissingAstronaut.Id).Throws<ArgumentException>();
+        var controller = new AstronautController(service, _mapper);
+        
+        var result = await controller.DeleteAstronaut(MissingAstronaut.Id);
+
+        result.Should().BeOfType<BadRequestResult>();
+    }
+}
---
+++ Test.Unit/Helpers/AstronautHelpers.cs
@@ -0,0 +1,94 @@
+﻿using API.DTO;
+using API.Models;
+using AstronautDao = API.DAO.Astronaut;
+
+namespace Test.Unit.Helpers;
+
+public static class AstronautHelpers
+{
+    public static Astronaut FirstAstronaut = new()
+    {
+        Id = Guid.NewGuid(),
+        Name = "Name1",
+        Surname = "Surname1",
+        Birthday = DateTime.Now,
+        Ability = "Ability1"
+    };
+    
+    public static Astronaut SecondAstronaut = new()
+    {
+        Id = Guid.NewGuid(),
+        Name = "Name2",
+        Surname = "Surname2",
+        Birthday = DateTime.Now,
+        Ability = "Ability2"
+    };
+    
+    public static Astronaut MissingAstronaut = new()
+    {
+        Id = Guid.NewGuid(),
+        Name = "Name3",
+        Surname = "Surname3",
+        Birthday = DateTime.Now,
+        Ability = "Ability3"
+    };
+    
+    public static AstronautRequestModel ToAstronautRequest(Astronaut astronautDto) => new()
+    {
+        Name = astronautDto.Name,
+        Surname = astronautDto.Surname,
+        Ability = astronautDto.Ability,
+        Birthday = astronautDto.Birthday
+    };
+    
+    public static AstronautResponseModel ToAstronautResponse(Astronaut astronaut) => 
+        new() 
+        {
+            Id = astronaut.Id,
+            Name = astronaut.Name,
+            Surname = astronaut.Surname,
+            Birthday = astronaut.Birthday,
+            Ability = astronaut.Ability
+        };
+
+    public static AddAstronaut ToAddAstronaut(Astronaut astronaut) => 
+        new() 
+        {
+            Name = astronaut.Name,
+            Surname = astronaut.Surname,
+            Birthday = astronaut.Birthday,
+            Ability = astronaut.Ability
+        };
+    
+    public static AstronautAllResponseModel ToAstronautAllResponse(IEnumerable<Astronaut> astronauts) => 
+        new() 
+        {
+            Astroanuts = astronauts.Select(ToAstronautResponse)
+        };
+    
+    public static async IAsyncEnumerable<Astronaut> GetAstronautDtosAsync()
+    {
+        yield return FirstAstronaut;
+        await Task.Delay(200);
+        yield return SecondAstronaut;
+    }
+    
+    public static async IAsyncEnumerable<AstronautDao> GetAstronautDaosAsync()
+    {
+        yield return ToAstronautDao(FirstAstronaut);
+        await Task.Delay(200);
+        yield return ToAstronautDao(SecondAstronaut);;
+    }
+
+    private static AstronautDao ToAstronautDao(Astronaut astronautDto) => new()
+    {
+        Id = astronautDto.Id,
+        Pk = astronautDto.Id,
+        Name = astronautDto.Name,
+        Surname = astronautDto.Surname,
+        Ability = astronautDto.Ability,
+        Birthday = astronautDto.Birthday
+    };
+    
+
+}
