ac63a751385ef728973c3b363129addde7ff5b3f
Ivan Kiral <kiralivan@gmail.com>
2021-12-04 12:27:28 +0100

    Implement service

--- API/Services/AstronautService.cs
+++ API/Services/AstronautService.cs
@@ -1,27 +1,31 @@
 ﻿using API.DTO;
 using API.Repository;
+using API.Utils;
 
 namespace API.Services;
 
 public class AstronautService: IAstronautService
 {
-    public IAsyncEnumerable<Astronaut> GetALlAstronauts()
+    private readonly IAstronautRepository _repository;
+    
+    public AstronautService(IAstronautRepository repository)
     {
-        throw new NotImplementedException();
+        _repository = repository;
     }
 
-    public Task<Astronaut> AddAstronaut(AddAstronaut astronaut)
+    public IAsyncEnumerable<Astronaut> GetALlAstronauts() => _repository.GetAllAstronauts();
+
+    public async Task<Astronaut> AddAstronaut(AddAstronaut astronaut)
     {
-        throw new NotImplementedException();
+        var newAstronaut = AstronautUtils.MakeAstronaut(astronaut);
+
+        return await _repository.AddAstronaut(newAstronaut);
     }
 
-    public Task<Astronaut> UpdateAstronaut(Astronaut astronaut)
+    public async Task<Astronaut> UpdateAstronaut(Astronaut astronaut)
     {
-        throw new NotImplementedException();
+        return await _repository.UpdateAstronaut(astronaut);
     }
 
-    public Task DeleteAstronaut(Guid id)
-    {
-        throw new NotImplementedException();
-    }
+    public async Task DeleteAstronaut(Guid id) => await _repository.DeleteAstronaut(id);
 }
---
+++ API/Utils/AstronautUtils.cs
@@ -0,0 +1,15 @@
+﻿using API.DTO;
+
+namespace API.Utils;
+
+public static class AstronautUtils
+{
+    public static Astronaut MakeAstronaut(AddAstronaut astronaut) => new()
+    {
+        Id = Guid.NewGuid(),
+        Name = astronaut.Name,
+        Surname = astronaut.Surname,
+        Birthday = astronaut.Birthday,
+        Ability = astronaut.Ability
+    };
+}
