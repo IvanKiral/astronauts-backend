3a71658c4c16f286586c3ddb0478521bd2578e31
Ivan Kiral <kiralivan@gmail.com>
2021-12-04 09:53:50 +0100

    Add Service

---
+++ API/Services/AstronautService.cs
@@ -0,0 +1,27 @@
+﻿using API.DTO;
+using API.Repository;
+
+namespace API.Services;
+
+public class AstronautService: IAstronautService
+{
+    public IAsyncEnumerable<Astronaut> GetALlAstronauts()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Astronaut> AddAstronaut(AddAstronaut astronaut)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Astronaut> UpdateAstronaut(Astronaut astronaut)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAstronaut(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
