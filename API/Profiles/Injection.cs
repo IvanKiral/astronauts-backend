b2149d01d4d3f00f80a9d69153bcddc77bc293ef
Ivan Kiral <kiralivan@gmail.com>
2021-12-04 10:09:16 +0100

    Add Dependency injection and Automapper library

---
+++ API/Profiles/Injection.cs
@@ -0,0 +1,11 @@
+﻿using API.Services;
+
+namespace API.Profiles;
+
+public static class Injection
+{
+    public static void RegisterDependencies(this IServiceCollection services)
+    {
+        services.AddScoped<IAstronautService, AstronautService>();
+    }
+}
